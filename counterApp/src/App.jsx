69143c1d665f44e212ce0ebdc501e2c2184693e5
Melvin0070 <melvin.aspirant@gmail.com>
2023-11-29 18:36:02 +0530

    Done

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -1,28 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-import Incerement from '../src/Components/Incerement'
-import Decrement from '../src/Components/Decrement'
-import Reset from '../src/Components/Reset'
-
+import Counter from './Components/Counter';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1>Counter App</h1>
-
-      <h1>{count}</h1>
-
-      <div className='flexbox'>
-        <Incerement/>
-        <Decrement/>
-        <Reset/>
-      </div>
-      
-    </>
-  )
+      <Counter />
+    </div>
+  );
 }
-
 export default App
